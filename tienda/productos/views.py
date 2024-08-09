93cfaa87ef376a791cc1848613ec58785c8e8cf1
garcia0770 <ocamporestrepo23@gmail.com>
2024-08-08 20:53:02 -0500

    Lista de productos
    Creamos un programa para una pagina web de una tienda, utilizamos django, aca estamos guardando productos y clientes

---
+++ tienda/productos/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Cliente
+from .models import Producto
+
+# Create your views here.
+
+def listar_clientes(request): 
+    clientes = Cliente.objects.all()
+    return render(request, 'listar_clientes.html', {'clientes': clientes})
+
+def listar_productos(request): 
+    clientes = Producto.objects.all()
+    return render(request, 'listar_productos.html', {'productos': productos})
